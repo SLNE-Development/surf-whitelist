2f2190cc9bbc63ca97463dbb1b9d0c01a042eeab
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-18 15:33:38 +0200

    feat: rename social connections table for consistency

--- src/main/kotlin/dev/slne/surf/freebuild/whitelist/database/table/SocialsTable.kt
+++ src/main/kotlin/dev/slne/surf/freebuild/whitelist/database/table/SocialsTable.kt
@@ -4,7 +4,7 @@ import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.columns.time.offsetDateTime
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.dao.id.LongIdTable
 
-object SocialsTable : LongIdTable("social_connections_new") {
+object SocialsTable : LongIdTable("social_connections") {
     val discordUserId = long("discord_user_id").uniqueIndex()
     val minecraftUuid = nativeUuid("minecraft_uuid").uniqueIndex()
     val twitchId = long("twitch_id").uniqueIndex().nullable()
