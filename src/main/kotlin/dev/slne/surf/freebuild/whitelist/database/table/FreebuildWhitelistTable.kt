5b3632316f45caf999cb9545cdd3c75b21701f4c
TheBjoRedCraft <143264463+TheBjoRedCraft@users.noreply.github.com>
2026-07-23 21:48:38 +0200

    Update src/main/kotlin/dev/slne/surf/freebuild/whitelist/database/table/FreebuildWhitelistTable.kt

--- src/main/kotlin/dev/slne/surf/freebuild/whitelist/database/table/FreebuildWhitelistTable.kt
+++ src/main/kotlin/dev/slne/surf/freebuild/whitelist/database/table/FreebuildWhitelistTable.kt
@@ -4,7 +4,7 @@ import dev.slne.surf.database.columns.time.offsetDateTime
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.dao.id.LongIdTable
 import dev.slne.surf.database.table.AuditableLongIdTable
 
-object FreebuildWhitelistTable : AuditableLongIdTable("surf-whitelist.freebuild_whitelists") {
+object FreebuildWhitelistTable : AuditableLongIdTable("freebuild_whitelists") {
     val socialConnectionId =
         ulong("social_connection_id").references(SocialConnectionsTable.id).uniqueIndex()
     val blocked = bool("blocked").default(false)
